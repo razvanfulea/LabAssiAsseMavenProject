893e61e2191e3aced08e94425f1c35b9c1dc9acc
razvanfulea <razvanfulea@gmail.com>
2019-03-18 10:55:38 +0200

    Initial commit

---
+++ src/test/java/ro/ubbcluj/ServiceTest.java
@@ -0,0 +1,20 @@
+package ro.ubbcluj;
+
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class ServiceTest {
+
+    @Test
+    void test_addStudent(){
+        assertEquals(1, 1);
+    }
+
+    @Test
+    void test_addLabAssignment(){
+        assertEquals("a", "a");
+    }
+
+}
